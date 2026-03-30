9442fd261edd412187f54e790b46503eb4658a77
ajs0428 <ajstewart04@gmail.com>
2026-03-30 12:58:06 -0400

    big move to separate data processing from modeling

--- Shell_Scripts/test_dem.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash/ -l
-
-cd /ibstorage/anthony/NYS_Wetlands_GHG
-module load R/4.4.3
-
-
-Rscript R_Code_Analysis/DEM_Extract_singleVect_CMD.r "Data/NYS_DEM_Indexes/" "Data/NY_HUCS/NY_Cluster_Zones_200.gpkg" \
-        208 "Data/DEMs/" "Data/TerrainProcessed" >& test_dem.log &
